84ca0699d652fa224a94ed2ddbc68a8acd4a77b7
TheTechPriest <corentin.veran@xour.fr>
2026-01-19 01:07:54 +0100

    first commit

---
+++ color.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+// Creating a version of our vector class called 'color' containing a pixel's color info
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Turning the normalized vector coords into its rgb value equivalent (0-1 to 0-256) 
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+ 
+
+#endif
---
+++ main.cc
@@ -0,0 +1,23 @@
+#include "color.h"
+#include "vec3.h"
+
+#include <iostream>
+
+int main() {
+
+    int imageW = 256;
+    int imageH = 256;
+
+    std::cout << "P3\n" << imageW << ' ' << imageH << "\n255\n";
+
+    for (int j = 0; j < imageH; j++) {
+        std::clog << "\rScanlines remaining: " << (imageH - j) << ' ' << std::flush;
+        for (int i = 0; i < imageW; i++) {
+            auto pixel_color = color(double(i) / (imageW-1), double(j) / (imageH-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+
+}
